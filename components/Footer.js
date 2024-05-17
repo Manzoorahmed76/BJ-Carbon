664ab669ba3afd78f22c38e74086fe266cd60d16
Don't open it <160379580+Manzoorahmed76@users.noreply.github.com>
2024-05-17 03:35:23 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,35 +6,19 @@ import { COLORS } from '../lib/constants'
 const Footer = () => (
   <footer role="contentinfo" className="mt3">
     <nav className="mt3">
-      <Link href="/about" prefetch={false}>
-        <a className="link" href="/about">
-          about
-        </a>
-      </Link>
-      <a className="link" href="https://github.com/carbon-app/carbon">
-        source
-      </a>
-      <a className="link" href="/terms">
-        terms
-      </a>
-      <a className="link" href="/privacy">
-        privacy
-      </a>
-      <a className="link" href="/offsets">
-        offsets
-      </a>
+    
       {/* <span className="new">New</span> */}
     </nav>
 
     <div className="mt2 mb2">
       created by{' '}
-      <a className="author-link" href="https://twitter.com/carbon_app">
-        @carbon_app
+      <a className="author-link" href="https://t.me/Mr_Bablu07">
+        @Mr,BaBlU
       </a>{' '}
       ¬
     </div>
     <style jsx>
-      {`
+      {
         footer {
           font-size: 14px;
           text-align: center;
@@ -79,7 +63,7 @@ const Footer = () => (
         .author-link:hover {
           color: #fff;
         }
-      `}
+      }
     </style>
   </footer>
 )
